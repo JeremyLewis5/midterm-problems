81e9f1823fb78add1b8172702471f86d1a85b8c4
JeremyLewis5 <jeremy.lewis@codeimmersives.com>
2021-03-09 08:44:52 -0600

    change

--- main.js
+++ main.js
@@ -1,22 +1,77 @@
-function isEvenlyDivisible() {
+function isEvenlyDivisible(num1, num2) {
+ // let output = '';
+  // for (const number of arr) 
+  if(num1 % num2 === 0 ){
+    return true;
+  } else {
+    return false
+  }
+   
 }
 
-function halfSquare() {
+function halfSquare(num1) {
+  let halfSquared = num1 * num1 / 2;
+  if (halfSquare ) {
+    return halfSquared
+  } else {
+    return false
+  }
+
 }
 
-function isLong() {
+function isLong(str) {
+  if (str.length >= 15) {
+    return true
+  } else {
+    return false
+  }
 }
 
-function exclaim() {
+function exclaim(str) {
+//   let output = '';
+//   for (let i = str.length - 1; i < str.length; i--) {
+//     const char = str[str.length - 1];
+//     if (char === '') {
+//     output =+ '!';
+//     } else {
+//       return output;
+//     }
+    
+//   }
+//   return output;
+// }
+let i;
+
+  for (i = 0; i < str.length; i++) {
+    str[i] = str[i] + '!';
+  }
+
+  return str[i];
 }
 
-function countWords() {
+function countWords(str) {
+  return str.split(' ').length;
 }
 
-function containsDigit() {
+function containsDigit(str) {
+  const isDigit = ['0', '1', '2', '3', '4', '5',
+'6', '7', '8', '9']
+  if (str === isDigit) {
+    return true
+  } else {return false}
+  
 }
 
-function containsLowerCase() {
+function containsLowerCase(str) {
+  const char = ['a','b','c','d','e','f','g','h','i',
+  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  //const hasLow = char.toLowerCase();
+  if (str === char ) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function containsUpperCase() {
@@ -25,7 +80,13 @@ function containsUpperCase() {
 function containsNonAlphanumeric() {
 }
 
-function containsSpace() {
+function containsSpace(str) {
+  const spaced = " ";
+  if (str === spaced) {
+    return true
+  } else {
+    return false
+  }
 }
 
 function digits() {
